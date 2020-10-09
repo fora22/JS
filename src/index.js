cb68a32908bee7ad7736cb1e90ecc159c29d6b51
fora22 <48875566+fora22@users.noreply.github.com>
2020-10-09 20:06:11 +0900

    NomadCoders Homework #3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
-// <⚠️ DONT DELETE THIS ⚠️>
 import "./styles.css";
-// <⚠️ /DONT DELETE THIS ⚠️>
 
-const newTagHTML = document.createElement("div");
-newTagHTML.innerHTML = "Hello!";
+const textTitle = document.createElement("div");
+const timeD_DayZone = document.createElement("div");
+const enterText = document.createElement("br");
+
+textTitle.classList.add("text_title");
+textTitle.innerHTML = "Time Until Chrismas";
+textTitle.style.fontWeight = "bold";
+textTitle.append(enterText);
+
+timeD_DayZone.classList.add("timeZone");
+timeD_DayZone.style.fontWeight = "bold";
+
 const bodyHTML = document.querySelector("body");
-bodyHTML.append(newTagHTML);
-newTagHTML.style.color = "white";
+bodyHTML.prepend(timeD_DayZone);
+bodyHTML.prepend(textTitle);
 
-function ResizeWindowChangeColor() {
-  const windowWidthSize = window.innerWidth;
-  console.log(windowWidthSize);
-  if (windowWidthSize <= 700) {
-    document.bgColor = "dodgerblue";
-  } else if (windowWidthSize > 700 && windowWidthSize <= 850) {
-    document.bgColor = "mediumpurple";
-  } else if (windowWidthSize > 850) {
-    document.bgColor = "yellow";
-  }
+console.log(bodyHTML);
+// You're gonna need this -> 
+// well..I'm in Korea. So I think I don't need this..don't you? It is right?
+// oh...It need minus 9hour...sorry..
+const NINE_HOURS_MILLISECONDS = 32400000;
+
+function getTime() {
+  const nowDate = new Date();
+
+  // Don't delete this.
+  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+
+  const dateGap =
+    xmasDay.getTime() -
+    NINE_HOURS_MILLISECONDS -
+    (nowDate.getTime() - NINE_HOURS_MILLISECONDS);
+  const dateGapDay = Math.floor(dateGap / (1000 * 60 * 60 * 24));
+  const dateGpaHour = Math.floor(dateGap / (1000 * 60 * 60) - dateGapDay * 24);
+  const dateGapMinute = Math.floor(
+    dateGap / (1000 * 60) - dateGapDay * 60 * 24 - dateGpaHour * 60
+  );
+  const dateGapSecond = Math.floor(
+    dateGap / 1000 -
+      dateGapDay * 60 * 60 * 24 -
+      dateGpaHour * 60 * 60 -
+      dateGapMinute * 60
+  );
+
+  timeD_DayZone.innerHTML = `${
+    dateGapDay < 10 ? `0${dateGapDay}` : dateGapDay
+  }d ${dateGpaHour < 10 ? `0${dateGpaHour}` : dateGpaHour}h ${
+    dateGapMinute < 10 ? `0${dateGapMinute}` : dateGapMinute
+  }m ${dateGapSecond < 10 ? `0${dateGapSecond}` : dateGapSecond}s`;
 }
 
-window.addEventListener("resize", ResizeWindowChangeColor);
+function init() {
+  setInterval(getTime, 1000);
+}
+init();

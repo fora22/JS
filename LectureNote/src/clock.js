681dc772373c0fccf24fc1ef920941c3422aef29
fora22 <48875566+fora22@users.noreply.github.com>
2020-10-10 21:36:16 +0900

    폴더 정리 및 Homework #4 작성 완료

---
+++ LectureNote/src/clock.js
@@ -0,0 +1,16 @@
+const clockConatiner = document.querySelector(".js-clock"),
+  clockTitle = clockConatiner.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function init() {
+  setInterval(getTime, 1000);
+}
+
+init();

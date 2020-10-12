8a56cd6bd26042a8ea892b9ebf8ada695b731c4d
팽대원 <fora22@kyonggi.ac.kr>
2020-10-12 15:47:18 +0900

    lecture 3.5~3.7까지 끝냄(todo list 삭제)

--- LectureNote/src/todo.js
+++ LectureNote/src/todo.js
@@ -5,21 +5,53 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = 'toDos';
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (TODOS_LS === null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        // REVIEW  parseInt는 string을 int로 변환
+        return toDo.id !== parseInt(li.id);     // id가 같지 않은 것만 반환 JS에서는 !== or ===는 boolean형태가 아닌 그 해당 값(객체 등)을 반환하는 듯.
+    }); // REVIEW filter는 forEach처럼 각각의 Item을 해당 함수로 실행하는 것을 뜻함.
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    // REVIEW localstorage에서는 모든 value가 string으로 저장된다.
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // REVIEW JSON.stringify 는 object를 string으로 바꿔준다. 
+}
+
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "X";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "X";
     span.innerText = text;
-    li.appendChild(span);
+    delBtn.addEventListener("click", deleteToDo);
     li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {

02b6b6beafcb1de01b94bf2da39c018ff4a58d13
yiy829 <yiy829@gmail.com>
2022-08-06 01:40:04 +0900

    feat: 기능구현 완료

--- NodeJS_IY/7-challenge/app.js
+++ NodeJS_IY/7-challenge/app.js
@@ -1,19 +1,6 @@
-// const { stdin, mainModule } = require('process');
-// const { StringDecoder } = require('string_decoder');
-
 const path = require('path');
 const fs = require('fs');
-const { AsyncLocalStorage } = require('async_hooks');
 const ps = fs.promises;
-
-// fs.readdir(path.join(__dirname, 'test'), function(error, filelist){
-//     if (!error) {
-//         var fileList = filelist;
-//     } else {
-//         console.log("ERROR!! : ", error);
-//     }
-//     console.log(fileList);
-//   })
 
 // 경로 탐색하여 파일리스트 불러오기
 async function getFileList(folder) {
@@ -22,40 +9,40 @@ async function getFileList(folder) {
 }
 
 // 특정 경로 폴더 만들기
-async function makeDir(dirPath) {
-    ps.mkdir(dirPath, { recursive: true})
+async function makeDir() {
+    ps.mkdir('./test/video', {recursive: true})
+    .catch(console.error);
+    ps.mkdir('./test/captured', {recursive: true})
+    .catch(console.error);
+    ps.mkdir('./test/duplicated', {recursive: true})
     .catch(console.error);
 }
 
-
-if (path.extname(__filename) in ['.mov', '.mp4']) {
-
+// 파일 옮기기
+function moveFile(folderName, fileName) {
+    fs.promises
+    .rename('./test/' + fileName, './test/'+ folderName + '/' + fileName)
+    .catch(console.error);
 }
-if (path.extname(__filename) in ['.png', '.aae']) {
 
+async function main() {
+    await makeDir();
+    const fileList = await getFileList('test');
+    for (var i in fileList) {
+        var fileName = fileList[i];
+        if (['.mov', '.mp4'].indexOf(path.extname(fileName)) != -1) {
+            await moveFile('video', fileName);    
+        }
+        if (['.png', '.aae'].indexOf(path.extname(fileName)) != -1) {
+            await moveFile('captured', fileName);
+        }
+
+        // 이미지 파일 명은 _기준으로 나눠 E가 붙었는지 확인
+        var imageFile = path.basename(fileName, path.extname(fileName)).split('_')[1];
+        if (imageFile && !imageFile.includes('E')) {
+            await moveFile('duplicated', fileName);
+        }    
+    }
 }
-// 패턴 찾기
 
-// // reading a file
-// ps.readFile('./text.txt', 'utf8') //
-//   .then((data) => console.log(data))
-//   .catch(console.error);
-
-// // writing a file
-// ps.writeFile('./file.txt', 'Hello, Dream Coders! :) ') //
-//   .catch(console.error);
-
-// ps.appendFile('./file.txt', 'Yo!, Dream Coders! :) ') //
-//   .catch(console.error);
-
-// // copy
-// ps.copyFile('./file.txt', './file2.txt') //
-//   .catch(console.error);
-
-// // folder
-// ps.mkdir('sub-folder') //
-//   .catch(console.error);
-
-// ps.readdir('./') //
-//   .then(console.log)
-//   .catch(console.error);
+main();

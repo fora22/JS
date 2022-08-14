1887b3a9e66fc9a318b55025d6f9ee288bef103f
yiy829 <yiy829@gmail.com>
2022-08-14 23:40:35 +0900

    feat : stream 형식으로 변경

--- NodeJS_IY/9-node-server/1-http.js
+++ NodeJS_IY/9-node-server/1-http.js
@@ -1,32 +1,19 @@
 const http = require('http');
-// const http2 = require('http2'); // https certificate가 없으므로 배포시에만 변경할 것
+const fs = require('fs');
 
-// console.log(http.STATUS_CODES); // Status Code 확인
-// console.log(http.METHODS); // Methods 확인
-
-const server = http.createServer((req, res) => { // 서버 생성
+const server = http.createServer((req, res) => {
   const url = req.url;
-  if(url === '/') { // URL 경로
-    res.setHeader('Content-Type', 'text/html'); // 이건 그냥 text가 아니라 html이야
-    res.write('<html>');
-    res.write('<head><title>Academy</title></head>');
-    res.write('<body><h1>Welcome!</h1></body>');
-    res.write('</html>');
+  console.log(url);
+  res.setHeader('Content-Type', 'text/html');
+  if(url === '/') {
+    fs.createReadStream('./html/index.html').pipe(res);
   } else if(url === '/courses') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Hello</title></head>');
-    res.write('<body><h1>Courses</h1></body>');
-    res.write('</html>');
+    fs.createReadStream('./html/courses.html').pipe(res);
   } else {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Academy</title></head>');
-    res.write('<body><h1>Not Found!</h1></body>');
-    res.write('</html>');
+    fs.createReadStream('./html/not-found.html').pipe(res);
   }
   
-  res.end(); // 응답 종료
+  res.end();
 });
 
 server.listen(8080);

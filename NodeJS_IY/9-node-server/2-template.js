a3eb6c25d9b6397fd137a38730607e7c0df9b808
yiy829 <yiy829@gmail.com>
2022-08-15 02:01:10 +0900

    feat: JSON 형식 보내주는 기능 추가

--- NodeJS_IY/9-node-server/2-template.js
+++ NodeJS_IY/9-node-server/2-template.js
@@ -1,13 +1,15 @@
 const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
+// const http2 = require('http2'); // https
 
-const name = "Ellie";
+const name = "YIY";
 const courses = [
   { name: "HTML" },
   { name: "CSS" },
-  { name: "JavaScript" },
+  { name: "JS" },
   { name: "Node" },
+  { name: "Frontend" },
 ];
 
 const server = http.createServer((req, res) => {
@@ -15,19 +17,17 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
   if (url === "/") {
     ejs
-      .renderFile(".template/index.ejs", { name })
+      .renderFile("./template/index.ejs", { name })
       .then((data) => res.end(data));
   } else if (url === "/courses") {
     ejs
-      .renderFile(".template/courses.ejs", { courses })
+      .renderFile("./template/courses.ejs", { courses })
       .then((data) => res.end(data));
   } else {
     ejs
-      .renderFile(".template/not-found.ejs", { name })
+      .renderFile("./template/not-found.ejs", { name })
       .then((data) => res.end(data));
   }
-
-  res.end();
 });
 
 server.listen(8080);

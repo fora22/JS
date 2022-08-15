63057f794a02a16c88ec056a8784dafa9bce30b9
yiy829 <yiy829@gmail.com>
2022-08-15 17:02:32 +0900

    feat: router 기능

--- NodeJS_IY/11-express/7-routing/app3.js
+++ NodeJS_IY/11-express/7-routing/app3.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import userRouter from './routes/user.js';
-import postRouter from './routes/post.js';
+import express from "express";
+import userRouter from "./routes/user.js";
+import postRouter from "./routes/post.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
+// 다른 파일에 매핑할 수 있음.
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 
 app.listen(8080);

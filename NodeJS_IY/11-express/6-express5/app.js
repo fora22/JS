d6054fbd1c1900b68976e8e2fb60903d89e8ca20
yiy829 <yiy829@gmail.com>
2022-08-15 16:50:58 +0900

    feat: express 5.0.0 alpha로 비동기 에러 처리

--- NodeJS_IY/11-express/6-express5/app.js
+++ NodeJS_IY/11-express/6-express5/app.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import fsAsync from 'fs/promises';
+import express from "express";
+import fsAsync from "fs/promises";
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  return fsAsync.readFile('/file2.txt').catch(next);
+app.get("/", (req, res, next) => {
+  return fsAsync.readFile("/file2.txt").catch(next);
 });
 
 app.use((error, req, res, next) => {
   console.error(error);
-  res.status(500).json({ message: 'Something went wrong' });
+  res.status(500).json({ message: "Something went wrong" });
   next();
 });
 

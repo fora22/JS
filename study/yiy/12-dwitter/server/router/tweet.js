b030f406812b9492da290528f054eae42c2f583b
yiy829 <yiy829@gmail.com>
2022-08-19 23:21:00 +0900

    feat: server side REST API 구현

--- study/yiy/12-dwitter/server/router/tweet.js
+++ study/yiy/12-dwitter/server/router/tweet.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text: "드림코더분들 화이팅!",
@@ -26,7 +26,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const username = req.query.username; // 없다면 undefined
   const data = username
-    ? tweets.filter((t) => t.username === username)
+    ? tweets.filter((tweet) => tweet.username === username)
     : tweets;
   res.status(200).json(data);
 });
@@ -34,16 +34,46 @@ router.get("/", (req, res, next) => {
 // GET /tweets/:id
 router.get("/:id", (req, res, next) => {
   const id = req.params.id;
-  const tweet = tweets.find((t) => t.id === id);
+  const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
-    return res.status(404).json({ message: `Twweet id(${id}) not found` }); // sendStatus(404)는 json 보내지지 않음.
+    return res.status(404).json({ message: `Tweet id(${id}) not found` }); // sendStatus(404)는 json 보내지지 않음.
   }
 });
 
 // POST /tweets
+router.post("/", (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
 // PUT /tweets/:id
+router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
+
 // DELETE /tweets/:id
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
 
 export default router;

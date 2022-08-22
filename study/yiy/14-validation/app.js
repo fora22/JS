0ac81e0ce974f5757d07b4766e9cd4e903ca7aaf
InYoung Yeo <yiy829@gmail.com>
2022-08-22 09:49:34 +0900

    refactor : 공통 기능 리팩토링

--- study/yiy/14-validation/app.js
+++ study/yiy/14-validation/app.js
@@ -4,6 +4,15 @@ import { body, param, validationResult } from "express-validator";
 const app = express();
 app.use(express.json());
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  // 전체 에러메시지가 아닌 첫번째 에러메시지만 보내고 싶을 경우
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
+
 app.post(
   "/users",
   [
@@ -15,26 +24,18 @@ app.post(
     body("age").notEmpty().isInt().withMessage("숫자를 입력해"),
     body("email").isEmail().withMessage("이메일 입력해"),
     body("job.name").notEmpty(),
+    validate,
   ],
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     console.log(req.body);
-
     res.sendStatus(201);
   }
 );
 
 app.get(
   "/:email",
-  param("email").isEmail().withMessage("이메일 입력해"),
+  [param("email").isEmail().withMessage("이메일 입력해"), validate],
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     res.send("📧");
   }
 );
